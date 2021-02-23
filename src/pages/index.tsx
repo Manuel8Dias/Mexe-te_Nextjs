87e96d7c5b8bd1267de0770685469273da0666eb
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2021-02-23 18:47:25 +0000

    Commit Title e favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import { CompletedChallenges } from "../components/CompletedChallenges"
+import { Countdown } from "../components/countdown"
 import { ExperienceBar } from "../components/ExperienceBar"
 import { Profile } from '../components/Profile'
+
+import Head from "next/head"
 
 import styles from "../styles/pages/Home.module.css"
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Mexe-te</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
