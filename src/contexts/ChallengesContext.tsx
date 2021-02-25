ed907fa168e679423df6adf4ddbe6f6d108785c5
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2021-02-25 16:37:56 +0000

    Commit Notificação Web

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react'
+import { createContext, useState, ReactNode, useEffect } from 'react'
 import challenges from  '../../challenges.json'
 
 interface  Challenge {
@@ -32,15 +32,25 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
-    function levelUp() {
+    useEffect(() => {
+        Notification.requestPermission()
+    }, [])
+
+    function levelUp() { 
         setLevel(level + 1)
     }
 
-    function startNewChallenge() {
+    function startNewChallenge() {  
         const radomChallengeIndex = Math.floor (Math.random() * challenges.length)
         const challenge = challenges[radomChallengeIndex]
 
         setActiveChallenge(challenge)
+
+        if(Notification.permission === 'granted') {
+            new Notification('Novo desafio 🎉 ', {
+                body: `Vale ${challenge.amount}xp!`
+            })
+        }
     }
 
     function resetChallenge() {
